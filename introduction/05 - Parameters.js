7e5310a76a0f180124ea5b1ff44db4653dd319e9
Guilherme <Fireguiquenbr@gmail.com>
2023-02-10 17:47:44 -0300

    Update 05 - Parameters.js

--- introduction/05 - Parameters.js
+++ introduction/05 - Parameters.js
@@ -1,8 +1,20 @@
-// Em algum momento você pode se perguntar como passar 
-
+// Definindo um valor padrão do parâmetro caso não haja argumentos
+/* 
+    No exemplo abaixo, caso você não defina um argumento para o parâmetro "idade" por exemplo,
+    o console irá retornar "idade não informada"
+*/
 function pessoa(nome = 'não informado', idade = 'não informada'){
-    console.log(nome, idade)
-} pessoa('paulo', 15)
+    console.log(`Nome: ${nome}, Idade: ${idade}`)
+} 
+pessoa('paulo');
 
 
-gfghdfh
+
+// O que acontece se colocar mais argumentos do que parâmetros
+function carro(nome, cor){
+    return `
+    Nome: ${nome}
+    Cor: ${cor}
+    `
+}
+console.log(carro('Tesla X', 'Vermelho'))
